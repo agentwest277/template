f9f55eb5b7ce4bc9387bc719d8d77e12586147d3
agentwest277 <time67301@gmail.com>
2025-10-13 22:09:15 +0200

    Update default_qwen_image.sh

--- default_qwen_image.sh
+++ default_qwen_image.sh
@@ -51,6 +51,8 @@ UNET_MODELS=(
 LORA_MODELS=(
     "https://huggingface.co/Alex583940/lora_iphone_qwen/resolve/main/Qwen-iPhone-V1.1.safetensors?download=true"
 	"https://huggingface.co/Alex583940/qwen_girl/resolve/main/qwen_MCNL_v1.0.safetensors"
+	"https://huggingface.co/Alex583940/qwen_girl/resolve/main/Qwen-MysticXXX-v1.safetensors?download=true"
+	"https://huggingface.co/Alex583940/qwen_girl/resolve/main/qwen_snofs.safetensors?download=true"
 )
 
 VAE_MODELS=(
